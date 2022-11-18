227186258596af326c8695e461ac9cbb12323a40
Roberto Costa Lima <robertogurgel@gmail.com>
2022-11-17 22:07:59 -0300

    Tela do favoritos mostrando os valores

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -21,7 +21,7 @@ export default function Carrinho()
                 // );
             });
     }, []);
-
+    
     return (
         <div align="center">
             <h1> 
--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,10 +10,33 @@ import {
     IconButton
 } from "@mui/material";
 
+import "./styles.css";
+
 import { Favorite } from "@mui/icons-material";
 
 export default function Favoritos(){
+    const [items, setItems] = React.useState([]);
+    React.useEffect(() => {
+        fetch('http://localhost:8000/favoritos')
+        .then(res => res.json())
+        .then(dados => {
+        setItems(dados);
+    });
+    }, []);
     return(
-        <h1>Teste OK</h1>
+        <div>
+            <h1>Favoritos</h1>
+            <Divider/>
+            { items.map(cadaItem => {
+                return (
+                    <div>
+                        <span>{cadaItem.nome}</span>
+
+                        <span>R$ {cadaItem.valor}</span>
+                    </div>
+                )
+            }) }
+        </div>
+
     )
 }
